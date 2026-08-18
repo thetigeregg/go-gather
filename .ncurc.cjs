100986ade196a2c68063f308ab2d42b7dd5b043f
thetigeregg <111239332+thetigeregg@users.noreply.github.com>
2026-08-18 09:55:13 +0200

    fix(ncu-config): resolve angular updates within pinned major (#44)
    Co-authored-by: tigeregg <tigeregg@users.noreply.github.com>

--- .ncurc.cjs
+++ .ncurc.cjs
@@ -7,6 +7,13 @@ const isAngularPackage = (name) =>
 
 module.exports = {
   ...base,
+  target: (name) => {
+    if (isAngularPackage(name)) {
+      return 'minor';
+    }
+
+    return base.target(name);
+  },
   reject: (name) => name === 'typescript',
   filterResults: (packageName, { upgradedVersionSemver }) => {
     if (!isAngularPackage(packageName)) {
